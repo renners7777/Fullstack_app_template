5c0f3c50c53eeefa7338ff37eb2623d5fc676002
Chris <104800006+renners7777@users.noreply.github.com>
2023-04-08 20:57:09 +0100

    Changes made to server.js

--- server.js
+++ server.js
@@ -2,21 +2,20 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 const MongoClient = require("mongodb").MongoClient
-const ejs = require("ejs")
 require('dotenv').config()
 
 let db,
     dbConnectionString = process.env.DB_STRING,//What does this mean? Grabbing variable from env file.
-    dbName = 'sample_analytics',
+    dbName = 'sample_restaurants',
     collection
 
 MongoClient.connect(dbConnectionString)
     .then(client => {
         console.log('Connected to database...')
         db = client.db(dbName)
-        collection = db.collection('accounts')
+        collection = db.collection('restaurants')
     })
 
     app.listen(process.env.PORT || PORT, () =>{
-        console.log(`Server running on Port = ${PORT}`)
+        console.log(`Server running on Port 5050...`)
     })

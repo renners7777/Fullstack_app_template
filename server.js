24630de025d18ef1f9c506725da00caae94f511b
Chris <104800006+renners7777@users.noreply.github.com>
2023-04-08 18:21:10 +0100

    MongoDB connection string added to .env file

--- server.js
+++ server.js
@@ -5,3 +5,14 @@ const MongoClient = require("mongodb").MongoClient
 const ejs = require("ejs")
 require('dotenv').config()
 
+let db,
+    dbConnectionString = process.env.DB_STRING,//What does this mean? Grabbing variable from env file.
+    dbName = 'sample_analytics',
+    collection
+
+MongoClient.connect(dbConnectionString)
+    .then(client => {
+        console.log('Connected to database...')
+        db = client.db(dbName)
+        collection = db.collection('accounts')
+    })

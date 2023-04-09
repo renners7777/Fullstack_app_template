2376888a278e01ee5b0ab757f16a50ea8385b95b
Chris <104800006+renners7777@users.noreply.github.com>
2023-04-09 16:21:56 +0100

    Added middleware

--- instructions.js
+++ instructions.js
@@ -7,15 +7,15 @@
 
 //4. Connect to MongoDB + add connection string to .env file
 
-//5. Add .env to .gitignore file
+//5. Create .gitignore file
 
-//6. Create Port
+//6. Add .env to .gitignore file
+
+//7. Create Port
 
 //---test mongo and port connection
 
-//7. Set Middleware
-
-//8. Create .gitignore file
+//8. Set Middleware
 
 //9. Create public and views folders - add main.js + style.css to Public and index.ejs to Views
 
--- server.js
+++ server.js
@@ -21,6 +21,15 @@ MongoClient.connect(dbConnectionString)
         console.log(err)
     })
 
+    app.set('view engine', 'ejs')
+    app.use(express.static('public'))
+    app.use(express.urlencoded((_extended) =>{
+        return true
+    }))
+    app.use(express.json())
+    app.use(cors())
+
+    //PORT 5050
     app.listen(process.env.PORT || PORT, () =>{
         console.log(`Server running on Port 5050...`)
     })
